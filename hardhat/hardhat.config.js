615e59400d890fdaa6bdad94df8a86932af1992e
augustinenwike <augustineudo03@gmail.com>
2022-05-25 13:12:34 +0100

    initial

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [MUMBAI_PRIVATE_KEY],
+    },
+  },
+};
+
+// LW3Punks Contract Address: 0x746ad2Ba3155C4DD9A50B63BCC65518dbB5e7443
